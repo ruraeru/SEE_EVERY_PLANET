94fc8e7f1c045381d1dd6b50c96cf1c1186a58c5
xodn_ghkd <hwa1023@mjc.ac.kr>
2025-01-01 19:42:31 +0900

    NASA API Loading....

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "apod.nasa.gov",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
